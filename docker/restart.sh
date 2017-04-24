554f32ae6763aba8205d5cfc7951143a55d880d6
Andrey F. Mindubaev <covex.mobile@gmail.com>
2017-04-24 13:13:48 +0300

    Fix db migrate

--- docker/restart.sh
+++ docker/restart.sh
@@ -11,5 +11,7 @@ cp -r ''"$DEPLOY_DIRECTORY"'_tmp' $DEPLOY_DIRECTORY
 cd $DEPLOY_DIRECTORY
 docker login -u gitlab-ci-token -p $CI_JOB_TOKEN $CI_REGISTRY
 docker-compose up -d --build
+docker-compose exec -T php php bin/console --env=prod doctrine:migrations:status
+docker-compose exec -T php php bin/console --env=prod --no-interaction --allow-no-migration doctrine:migrations:migrate
 
 rm -rf ''"$DEPLOY_DIRECTORY"'_tmp'
