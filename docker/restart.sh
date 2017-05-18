88e2dcf819403e9c6515411e9141d3f352a63b39
Andrey F. Mindubaev <covex.mobile@gmail.com>
2017-05-18 12:27:24 +0300

    Pull image updates

--- docker/restart.sh
+++ docker/restart.sh
@@ -2,17 +2,16 @@
 
 if [ -d $DEPLOY_DIRECTORY ]; then
     cd $DEPLOY_DIRECTORY
-    docker-compose stop
-    docker-compose rm -f
+    docker-compose down --rmi local --remove-orphans
     rm -rf $DEPLOY_DIRECTORY
 fi
 
 cp -r ''"$DEPLOY_DIRECTORY"'_tmp' $DEPLOY_DIRECTORY
 cd $DEPLOY_DIRECTORY
 docker login -u gitlab-ci-token -p $CI_JOB_TOKEN $CI_REGISTRY
+docker-compose pull
 docker-compose build --force-rm --pull
 docker-compose up -d --remove-orphans
-#sleep 60
 docker-compose exec -T php phing app-update -Dsymfony.env=prod
 
 rm -rf ''"$DEPLOY_DIRECTORY"'_tmp'
