2bb38a21c940bc8ff8f69457d853c16c8619878c
Andrey F. Mindubaev <covex.mobile@gmail.com>
2017-06-19 23:36:02 +0300

    Minimize downtime

--- docker/restart.sh
+++ docker/restart.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
+docker login -u gitlab-ci-token -p $CI_JOB_TOKEN $CI_REGISTRY
+docker-compose pull
+
 if [ -d $DEPLOY_DIRECTORY ]; then
     cd $DEPLOY_DIRECTORY
-    docker-compose down --rmi local --remove-orphans
+    docker-compose down --rmi local
     rm -rf $DEPLOY_DIRECTORY
 fi
 
 cp -r ''"$DEPLOY_DIRECTORY"'_tmp' $DEPLOY_DIRECTORY
 cd $DEPLOY_DIRECTORY
-docker login -u gitlab-ci-token -p $CI_JOB_TOKEN $CI_REGISTRY
-docker-compose pull
-docker-compose build --force-rm --pull
+
 docker-compose up -d --remove-orphans
 docker-compose exec -T php phing app-deploy -Dsymfony.env=prod
 
