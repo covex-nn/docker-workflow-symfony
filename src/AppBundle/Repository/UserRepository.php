a8acbed14e7be8243de80a03f8f8dba9f485d41e
Andrey F. Mindubaev <covex.mobile@gmail.com>
2017-08-08 16:57:55 +0300

    User management

---
+++ src/AppBundle/Repository/UserRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AppBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+use AppBundle\Entity;
+
+/**
+ * User Repository
+ */
+class UserRepository extends EntityRepository
+{
+
+}
